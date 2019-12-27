238ec57c308a5aa07f891c485aff84d1d19a5124
Yosuke.N <yosuke.nozaki@repro.io>
2019-12-27 14:04:16 +0900

    first

---
+++ kokokara-folder/app/src/main/java/com/scrumd/kokokara_hajimeru/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.scrumd.kokokara_hajimeru.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
